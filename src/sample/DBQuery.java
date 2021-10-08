ad8e8df5747d39fb1105b655fc1332b35b69af66
cwins <cwinston7g@gmail.com>
2021-10-08 19:21:59 -0400

    2ND commit

--- src/sample/DBQuery.java
+++ src/sample/DBQuery.java
@@ -31,38 +31,6 @@ public class DBQuery {
         return contactsIDList;
     }
 
-    /* public static ObservableList<Contacts> GetContactList() throws SQLException {
-            List<List<String>> contact_data = GetResultsFromQuery("SELECT * FROM contacts");
-            List<Contacts> contact_list = new ArrayList<>();
-
-            for (List<String> val : contact_data) {
-                contact_list.add(new Contact(val.get(0), val.get(1), val.get(2)));
-            }
-
-            return FXCollections.observableList(contact_list);
-        }*/
-   /*public static ObservableList<Contacts> getContactsList() {
-       ObservableList<Contacts> contactsList = FXCollections.observableArrayList();
-       Connection conn = DBConnection.getConnection();
-       String query = "SELECT * FROM contacts";
-       Statement st;
-       ResultSet rs;
-       try {
-           st = conn.createStatement();
-           rs = st.executeQuery(query);
-           Contacts contact;
-           while (rs.next()) {
-            contact = new Contacts(rs.getInt("Contact_ID"),
-                    rs.getString("Contact_Name"),
-                    rs.getString("Email"));
-                    contactsList.add(contact);
-           }
-       } catch (Exception ex) {
-           ex.printStackTrace();
-       }
-       return contactsList;
-   }
-*/
    //ObservableList<String> contactsNameList = FXCollections.observableArrayList();
    public static ObservableList<Integer> contactsIDList = FXCollections.observableArrayList();
 
